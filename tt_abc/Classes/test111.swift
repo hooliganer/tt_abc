b8f24a6f13fc9a8cd4484aba190ba218657f2227
tantao <taoerist@gmail.com>
2024-03-11 18:35:53 +0800

    改

---
+++ tt_abc/Classes/test111.swift
@@ -0,0 +1,20 @@
+//
+//  test111.swift
+//  test
+//
+//  Created by 谭滔 on 2024-03-11.
+//
+
+import Foundation
+
+func funccc() {
+    print("??")
+}
+
+class Classssss: NSObject {
+    static let shared = Classssss()
+    
+    func printttt() {
+        funccc()
+    }
+}
